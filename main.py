6e1cb87e771258eb64759d3d0167f097219d14b4
Collin Schulte <schultec99@gmail.com>
2023-07-14 17:40:13 -0700

    added project content

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-
+import feedparser
+import bs4
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # take in url
+    url = input("please enter a rss feed url: ")
+    # parse feed
+    feed = feedparser.parse(url)
+    for entry in feed['entries']:
+        print("--------------------")
+        print(entry['title'])
+        # parse and display description
+        desc = entry['description']
+        desc = bs4.BeautifulSoup(desc, features="html.parser")
+        desc = desc.get_text()
+        print(desc)
+        # display link
+        print(entry['link'])
